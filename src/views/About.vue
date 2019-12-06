2f19bc8938a67984cedf4e8f8660a6b28fa526fa
Valentin U <valentin@kangaroorewards.com>
2019-12-06 08:45:03 -0500

    Add routes

---
+++ src/views/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>Contact us</h1>
+    <form name="contact" method="POST" data-netlify="true" data-netlify-recaptcha="true">
+      <p>
+        <label>Your Name: <input type="text" name="name" class="form-control" /></label>   
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" class="form-control"/></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message" class="form-control"></textarea></label>
+      </p>
+      <div data-netlify-recaptcha="true"></div>
+      <p>
+        <button type="submit" class="btn btn-primary">Send</button>
+      </p>
+    </form>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <Multiplication msg="*"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Multiplication from '@/components/Multiplication.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Multiplication
+  }
+}
+</script>
